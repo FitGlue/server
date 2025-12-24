8d262fa5223f8540800041d2eb730dbd4baedd79
James King <ripixel@gmail.com>
2025-12-24 23:03:54 +0000

    refactor: change userId declaration from const to let

--- src/typescript/shared/src/framework/index.ts
+++ src/typescript/shared/src/framework/index.ts
@@ -115,7 +115,8 @@ export function createCloudFunction(handler: FrameworkHandler, options?: CloudFu
     const serviceName = process.env.K_SERVICE || 'unknown-function';
 
     // Extract metadata from request (handles both HTTP and Pub/Sub)
-    const { userId, testRunId, triggerType } = extractMetadata(req);
+    let { userId } = extractMetadata(req);
+    const { testRunId, triggerType } = extractMetadata(req);
 
     // Initial Logger (Pre-Auth)
     const preambleLogger = logger.child({});
