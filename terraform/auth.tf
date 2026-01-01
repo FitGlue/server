2fae350b0c05a7931bbf63917a106137edd810cb
James King <ripixel@gmail.com>
2026-01-01 23:41:28 +0000

    fix: use proper identity platform resource

--- terraform/auth.tf
+++ terraform/auth.tf
@@ -1,11 +1,11 @@
-resource "google_identity_platform_project_default_config" "default" {
+resource "google_identity_platform_config" "default" {
   project = var.project_id
 
   sign_in {
     allow_duplicate_emails = false
 
     email {
-      enabled = true
+      enabled           = true
       password_required = true
     }
   }
