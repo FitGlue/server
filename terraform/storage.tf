ae26975b2ec7728bef48c64158e516684852faa8
James King <ripixel@gmail.com>
2026-01-17 21:00:46 +0000

    feat: add combined version control between web and server

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -13,3 +13,29 @@ resource "google_storage_bucket" "artifacts_bucket" {
     }
   }
 }
+
+# Version config bucket - stores unified FitGlue version across web/server repos
+resource "google_storage_bucket" "version_config_bucket" {
+  name     = "${var.project_id}-version-config"
+  location = var.region
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+# Grant CI service account access to read/write version config
+resource "google_storage_bucket_iam_member" "version_config_ci_access" {
+  bucket = google_storage_bucket.version_config_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:circleci-deployer@${var.project_id}.iam.gserviceaccount.com"
+}
+
+# Grant Web CI service account read access to version config
+resource "google_storage_bucket_iam_member" "version_config_web_ci_access" {
+  bucket = google_storage_bucket.version_config_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:circleci-web-deployer@${var.project_id}.iam.gserviceaccount.com"
+}
